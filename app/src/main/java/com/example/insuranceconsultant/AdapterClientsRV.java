c5340cd39effe16d76413cd7980ace86e2c7e6dd
YevheniiYeromenko <bicher09@gmail.com>
2020-10-13 19:31:29 +0300

    RecyclerView add

---
+++ app/src/main/java/com/example/insuranceconsultant/AdapterClientsRV.java
@@ -0,0 +1,58 @@
+package com.example.insuranceconsultant;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class AdapterClientsRV extends RecyclerView.Adapter<AdapterClientsRV.ClientInfoHolder> {
+    private List<Object> list;
+    private Context context;
+
+    public AdapterClientsRV(List<Object> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ClientInfoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.rv_item_client, parent, false);
+        return new ClientInfoHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClientInfoHolder holder, int position) {
+        holder.tvClinetName.setText("Yeromenko" + position);
+        Glide.with(context)
+                .load("https://media.licdn.com/dms/image/C4E0BAQGvfZGd31Hw_Q/company-logo_200_200/0?e=2159024400&v=beta&t=zXUgvlTYqfO4cCIr5kggBOcypfhK4i4pbMtzq6dbeP8")
+                .into(holder.imLifeInsCompany);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ClientInfoHolder extends RecyclerView.ViewHolder {
+        ImageView imLifeInsCompany;
+        TextView tvClinetName;
+
+        public ClientInfoHolder(@NonNull View itemView) {
+            super(itemView);
+            imLifeInsCompany = itemView.findViewById(R.id.imLifeInsCompany);
+            tvClinetName = itemView.findViewById(R.id.tvClinetName);
+        }
+    }
+}
--- app/src/main/java/com/example/insuranceconsultant/FirstActivity.java
+++ app/src/main/java/com/example/insuranceconsultant/FirstActivity.java
@@ -1,14 +1,37 @@
 package com.example.insuranceconsultant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private CardView cvClients;
+    private CardView cvTeam;
+    private ImageView imMetlife;
+    private ImageView imStarlife;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        cvClients = findViewById(R.id.cvClients);
+        cvTeam = findViewById(R.id.cvTeam);
+        imMetlife = findViewById(R.id.imMetlife);
+        imStarlife = findViewById(R.id.imStarlife);
+
+        cvClients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ClientsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
